f34a3675c9deef1679173f629830b40359631f8f
Javlon Bakhodirov <bakhodirov0428@gmail.com>
2024-03-16 17:39:54 +0500

    update option

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <!-- <TheWelcome /> -->
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  border-radius: 10px;
+  background-color: rgb(211, 211, 211);
+}
+
+.logo {
+  display: block;
+  margin: auto;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: grid;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  /* .logo {
+    margin: 0 2rem 0 0;
+  } */
+
+  header .wrapper {
+    display: grid;
+    place-items: flex-start;
+    /* flex-wrap: wrap; */
+  }
+}
+</style>
