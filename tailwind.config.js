64bfb877cab5ce49aa3ecf2dd4a2cfb1179aeee6
Javlon Bakhodirov <bakhodirov0428@gmail.com>
2024-03-28 17:58:19 +0500

    resume add

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/index.css"],
+  content: ["./src/**/*.{ts,js,tsx,jsx}"],
   theme: {
+    container: {
+      center: true,
+      padding: "1rem",
+      width: "1280px",
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
